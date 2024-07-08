35a96ef3508bed206c59169736a20ab293cb0fa3
Daniel Larsson <eadaniel.larsson@gmail.com>
2024-07-08 12:15:27 +0200

    Reverting to originial baseURL, with proxy in backend

--- frontend/swapi-app-react/src/services/api-client.ts
+++ frontend/swapi-app-react/src/services/api-client.ts
@@ -1,19 +1,7 @@
 import axios, { AxiosError, CanceledError } from "axios";
 
 export default axios.create({
-  // baseURL: 'https://swapi-app-production.up.railway.app/api/people'
-  baseURL: 'https://swapi-app-production.up.railway.app'
+  baseURL: 'https://swapi-app-production.up.railway.app/api/people'
 });
 
 export { AxiosError, CanceledError };
-
-async function fetchPeople() {
-  try {
-    const response = await axios.get('/api/people');
-    console.log(response.data);
-  } catch (error) {
-    console.error('Error fetching people:', error);
-  }
-}
-
-fetchPeople();
