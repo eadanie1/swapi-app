27f57707b4137254a8412d98ae261f41628c8891
Daniel Larsson <eadaniel.larsson@gmail.com>
2024-07-08 12:10:05 +0200

    Editing baseURL to match backend

--- frontend/swapi-app-react/src/services/api-client.ts
+++ frontend/swapi-app-react/src/services/api-client.ts
@@ -2,14 +2,14 @@ import axios, { AxiosError, CanceledError } from "axios";
 
 export default axios.create({
   // baseURL: 'https://swapi-app-production.up.railway.app/api/people'
-  baseURL: 'https://swapi-app-production.up.railway.app/api'
+  baseURL: 'https://swapi-app-production.up.railway.app'
 });
 
 export { AxiosError, CanceledError };
 
 async function fetchPeople() {
   try {
-    const response = await axios.get('/people');
+    const response = await axios.get('/api/people');
     console.log(response.data);
   } catch (error) {
     console.error('Error fetching people:', error);
