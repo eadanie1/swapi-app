c80cb9cd548a1835c101e6fa3c3fa3155f12b5ed
eadanie1 <146831722+eadanie1@users.noreply.github.com>
2025-04-27 00:34:23 +0200

    Update api-client.ts

--- frontend/swapi-app-react/src/services/api-client.ts
+++ frontend/swapi-app-react/src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, CanceledError } from "axios";
 
 export default axios.create({
-  baseURL: 'https://swapi-app-production.up.railway.app/api/people'
+  baseURL: 'https://swapi-app-02a7.onrender.com/api/people'
 });
 
 export { AxiosError, CanceledError };
